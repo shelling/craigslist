554b0089b64965845a9de3ff7f1988a968ef8d11
shelling <navyblueshellingford@gmail.com>
2015-08-22 13:42:31 -0700

    Initial commit. Add package.json, webpack.config.js, main.js

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var config = module.exports = {
+  context: __dirname,
+
+  entry: {
+    application: ["./src/main.js"],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+
+  resolve: {
+    extensions: ["", ".js", ".jsx"],
+    moduleDirectories: ["node_modules"],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'jsx',
+      },
+    ]
+  },
+};
