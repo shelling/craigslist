ecf7db7450df122451f51adcf6c143aa2a82a5ff
shelling <navyblueshellingford@gmail.com>
2015-08-22 14:49:53 -0700

    Add first Parse.Object. Add index.html

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 var Parse = require("Parse");
+var React = require("react/addons");
 
-module.exports = "main";
+Parse.initialize("rbxxqa1OqVDFrggjPndedyxSO3UzH6OETZ7ka4xp", "N7ZxmgK8dBCjfzUFoQnR9fOihO4dNopVZ08VyctS");
+
+var Main = Parse.Object.extend("Main", {
+  initialize: function(attrs, options) {
+  },
+});
+
+var m = new Main();
+
+m.set("name", "shelling");
+m.set("age", 29);
+
+m.save(null, {
+  success: function() {
+    console.log("success");
+  },
+  error: function() {
+    console.log("error");
+  },
+});
+
+module.exports = Main;
